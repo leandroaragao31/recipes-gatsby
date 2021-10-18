7d94674ff88de39492244341b889df7dcd70ca1b
luanpires94 <luan.94pires@gmail.com>
2021-10-18 19:13:50 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ module.exports = {
     {
     resolve: "gatsby-source-graphql",
       options: {
-        typeName: "alldata",
-        fieldName: "alldata",
-        url: "https://api-us-east-1.graphcms.com/v2/cku8rn0m642yc01yz16vd5076/master",
+        typeName: "",
+        fieldName: "",
+        url: "",
 
     }
   }
